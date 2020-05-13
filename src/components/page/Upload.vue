5fce2d903ef4e4852b087325010f946d1183907b
zzh <you@example.com>
2020-05-13 15:32:30 +0800

    zzh

--- src/components/page/Upload.vue
+++ src/components/page/Upload.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 表单</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 测试表单</el-breadcrumb-item>
                 <el-breadcrumb-item>图片上传</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
